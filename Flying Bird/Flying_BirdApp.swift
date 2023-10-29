bcea263c5962246b7785a4d399337eed6fec75bf
MohamedGoush <0x1110111@gmail.com>
2023-10-29 22:52:16 +0530

    Initial Commit

---
+++ Flying Bird/Flying_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_BirdApp.swift
+//  Flying Bird
+//
+//  Created by python on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Flying_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
