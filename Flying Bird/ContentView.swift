9f5a54249ac306a84a465e01c3361c28e597d888
MohamedGoush <0x1110111@gmail.com>
2023-10-29 23:22:25 +0530

    Bird Flying Animation Completed

--- Flying Bird/ContentView.swift
+++ Flying Bird/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var leftWing = false
+    @State private var rightWing = false
+    @State private var birdBody = false
+    @State private var animateShadow = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.0, green: 0.0, blue: 0.0)
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack{
+                    
+                //MARK: LEFT WING
+                Image("leftWing")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .shadow(color: .red, radius: 1, x: 0, y: 3)
+                    .rotationEffect(.degrees(leftWing ? -100 : 0), anchor: .bottomTrailing)
+                    .offset(x: -55, y: leftWing ? -190 : 0)
+                    .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true))
+                
+                //MARK: RIGHT WING
+                Image("rightWing")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .shadow(color: .blue, radius: 1, x: 0, y: 3)
+                    .rotationEffect(.degrees(rightWing ? 100 : 0), anchor: .bottomLeading)
+                    .offset(x: 55, y: rightWing ? -190 : 0)
+                    .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true))
+                
+                //MARK: Body
+                Image("body")
+                    .resizable()
+                    .offset(y: -30)
+                    .frame(width: 100, height: 125)
+                    .shadow(color: .white, radius: 1, x: 0, y: 0)
+                    .offset(y: birdBody ? -50 : 75)
+                    .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true))
+            }
+            .shadow(color: .white, radius: animateShadow ?  1 : 4, x: 0, y: animateShadow ? 10 : 1)
+            .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true))
+            .onAppear{
+                birdBody.toggle()
+                leftWing.toggle()
+                rightWing.toggle()
+                animateShadow.toggle()
+            }
         }
-        .padding()
     }
 }
 
